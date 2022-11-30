c57bc98b6c1799664a80c43ea4c8d388190d9426
mohammad.yazdi <mohammad.yazdi@partsoftware.com>
2022-11-30 16:39:25 +0330

    -Packaging Quote feature -Create QuoteScreen (#5)

---
+++ app/src/main/java/com/example/programmingquotes/feature/quote/ui/component/QuotesListItem.kt
@@ -0,0 +1,36 @@
+package com.example.programmingquotes.feature.quote.ui.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QuoteListItem(
+    modifier: Modifier = Modifier,
+    quote: String,
+    onClick: () -> Unit
+) {
+    Text(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(
+                border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.primary),
+                shape = MaterialTheme.shapes.medium
+            )
+            .clip(shape = MaterialTheme.shapes.medium)
+            .padding(all = 22.dp)
+            .clickable { onClick() }
+            .then(modifier),
+        text = quote,
+        style = MaterialTheme.typography.body1,
+        color = MaterialTheme.colors.primary
+    )
+}
