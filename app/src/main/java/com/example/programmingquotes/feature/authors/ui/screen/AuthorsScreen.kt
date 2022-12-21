9ce45b4da8e8c8846516858e204f10ea5904b7be
mohammad.taghizadeh <mohammad.taghizadeh@partsoftware.com>
2022-12-21 15:37:08 +0330

    #24- Bottom sheet optimized

--- app/src/main/java/com/example/programmingquotes/feature/authors/ui/screen/AuthorsScreen.kt
+++ app/src/main/java/com/example/programmingquotes/feature/authors/ui/screen/AuthorsScreen.kt
@@ -3,15 +3,16 @@ package com.example.programmingquotes.feature.authors.ui.screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,7 @@ import com.example.programmingquotes.core.ui.component.CustomButton
 import com.example.programmingquotes.feature.authors.ui.component.AppBar
 import com.example.programmingquotes.feature.authors.ui.component.AuthorListItem
 import com.example.programmingquotes.feature.authors.ui.component.BottomSheet
+import com.example.programmingquotes.feature.authors.ui.model.AuthorView
 import com.example.programmingquotes.feature.authors.ui.viewmodel.AuthorViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -35,61 +37,99 @@ internal fun AuthorsScreen(
     viewModel: AuthorViewModel
 ) {
     val scaffoldState = rememberScaffoldState()
-    BottomSheet(
-        scaffoldState = scaffoldState,
-        viewModel = viewModel
-    ) { bottomSheetState, scope ->
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            scaffoldState = scaffoldState,
-            topBar = {
-                AppBar()
-            },
-            content = {
-                Body(
-                    bottomSheetState = bottomSheetState,
-                    scope = scope,
-                    navController = navController,
-                    scaffoldState = scaffoldState,
-                    viewModel = viewModel
-                )
-            },
-            floatingActionButton = {
-                CustomButton(
-                    modifier = Modifier
-                        .width(188.dp),
-                    onClick = {
-                        scope.launch {
-                            bottomSheetState.show()
-                        }
-                    },
-                    title = stringResource(id = R.string.label_generate_random)
-                )
-            },
-            floatingActionButtonPosition = FabPosition.Center
-        )
-    }
-}
-
-@Composable
-private fun Body(
-    bottomSheetState: ModalBottomSheetState,
-    scope: CoroutineScope,
-    navController: NavController,
-    scaffoldState: ScaffoldState,
-    viewModel: AuthorViewModel
-) {
-    val pageState = viewModel.pageState.collectAsState().value
-    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val pageStateBottomSheet by viewModel.pageStateBottomSheet.collectAsState()
+    val pageState by viewModel.pageState.collectAsState()
     val pullRefreshState = rememberPullRefreshState(
         refreshing = pageState is ResultWrapper.Loading,
         onRefresh = { viewModel.getAuthors(true) }
     )
+    val bottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+    )
 
-    LaunchedEffect(key1 = pageState) {
-        if (pageState is Errors) {
+    ModalBottomSheetLayout(
+        modifier = Modifier.fillMaxSize(),
+        sheetState = bottomSheetState,
+        scrimColor = Color.Transparent,
+        sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        sheetElevation = 15.dp,
+        sheetContent = {
+            BottomSheet(
+                viewModel = viewModel,
+                pageStateBottomSheet = { pageStateBottomSheet },
+                scaffoldState = scaffoldState,
+                bottomSheetState = bottomSheetState
+            )
+        },
+        content = {
+            MainContent(
+                scaffoldState = scaffoldState,
+                navController = navController,
+                pullRefreshState = pullRefreshState,
+                pageState = pageState,
+                scope = scope,
+                bottomSheetState = bottomSheetState
+            )
+        },
+    )
+}
+
+@Composable
+private fun MainContent(
+    scaffoldState: ScaffoldState,
+    navController: NavController,
+    pullRefreshState: PullRefreshState,
+    pageState: ResultWrapper<List<AuthorView>>,
+    scope: CoroutineScope,
+    bottomSheetState: ModalBottomSheetState
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBar()
+        },
+        content = { padding ->
+            Body(
+                navController = navController,
+                scaffoldState = scaffoldState,
+                paddingValues = padding,
+                pullRefreshState = pullRefreshState,
+                pageState = { pageState }
+            )
+        },
+        floatingActionButton = {
+            CustomButton(
+                modifier = Modifier
+                    .width(188.dp),
+                onClick = {
+                    scope.launch {
+                        bottomSheetState.show()
+                    }
+                },
+                title = stringResource(id = R.string.label_generate_random)
+            )
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    )
+}
+
+@Composable
+private fun Body(
+    navController: NavController,
+    scaffoldState: ScaffoldState,
+    paddingValues: PaddingValues,
+    pullRefreshState: PullRefreshState,
+    pageState: () -> ResultWrapper<List<AuthorView>>
+) {
+    val context = LocalContext.current
+    val state = pageState()
+
+    LaunchedEffect(key1 = state) {
+        if (state is Errors) {
             scaffoldState.snackbarHostState.showSnackbar(
-                context.getMessageFromStringOrStringId(pageState.message)
+                context.getMessageFromStringOrStringId(state.message)
             )
         }
     }
@@ -98,8 +138,9 @@ private fun Body(
         modifier = Modifier
             .fillMaxSize()
             .pullRefresh(state = pullRefreshState)
+            .padding(paddingValues)
     ) {
-        if (pageState is ResultWrapper.Loading) {
+        if (state is ResultWrapper.Loading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         } else {
             LazyColumn(
@@ -109,8 +150,8 @@ private fun Body(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 contentPadding = PaddingValues(bottom = 80.dp)
             ) {
-                if (pageState is ResultWrapper.Success) {
-                    items(pageState.data) { authorView ->
+                if (state is ResultWrapper.Success) {
+                    items(state.data) { authorView ->
                         AuthorListItem(authorView) {
                             navController.navigate(Screens.QuotesScreen.withArg(authorView.name))
                         }
@@ -119,7 +160,7 @@ private fun Body(
             }
         }
         PullRefreshIndicator(
-            refreshing = pageState is ResultWrapper.Loading,
+            refreshing = state is ResultWrapper.Loading,
             state = pullRefreshState,
             modifier = Modifier.align(Alignment.TopCenter)
         )
