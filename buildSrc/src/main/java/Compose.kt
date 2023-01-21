588eb53b3bbb181dc351ec5881db0a31de7783dd
mohammad.yazdi <mohammad.yazdi@partsoftware.com>
2023-01-21 17:52:07 +0330

    #41 -create buildSrc -create AndroidX.kt and Compose.kt dependency file

---
+++ buildSrc/src/main/java/Compose.kt
@@ -0,0 +1,22 @@
+object Compose {
+
+    private const val activityComposeVersion = "1.6.1"
+    const val activity = "androidx.activity:activity-compose:$activityComposeVersion"
+
+    const val composeVersion = "1.3.2"
+    const val ui = "androidx.compose.ui:ui:$composeVersion"
+    const val toolingPreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
+    const val material = "androidx.compose.material:material:$composeVersion"
+
+    private const val navigationVersion = "2.5.3"
+    const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
+
+    private const val hiltNavigationComposeVersion = "1.1.0-alpha01"
+    const val hiltNavigation = "androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion"
+}
+
+object ComposeTest {
+    const val uiTestJunit4 = "androidx.compose.ui:ui-test-junit4:${Compose.composeVersion}"
+    const val uiTestManifest = "androidx.compose.ui:ui-test-manifest:${Compose.composeVersion}"
+    const val tooling = "androidx.compose.ui:ui-tooling:${Compose.composeVersion}"
+}
